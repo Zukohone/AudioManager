8cf6b1134a5df79d8a1b14b6cba3b463aa6e767c
Icaro <ikaro.d@live.com>
2014-04-01 14:49:44 -0300

    Initial Commit

---
+++ AudioManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AudioManager
+//
+//  Created by ICARO NASCIMENTO FERREIRA on 01/04/14.
+//  Copyright (c) 2014 ICARO NASCIMENTO FERREIRA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
